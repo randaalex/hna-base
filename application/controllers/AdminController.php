5c5f703b7b09f5349f1888b43f9489ad2a30f8e4
Alexander Randa <randa.alex@gmail.com>
2010-08-14 23:02:19 +0300

    Реализована аутентификация

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,75 @@
+<?php
+
+class AdminController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        $form = new Application_Form_Login();
+                $request = $this->getRequest();
+                if ($request->isPost()) {
+                    if ($form->isValid($request->getPost())) {
+                        if ($this->_process($form->getValues())) {
+                            // We're authenticated! Redirect to the home page
+                            $this->_helper->redirector('index', 'hna');
+                        } else {
+                            echo "Ошибка! Проверьте правильность введенных данных!";
+                        }
+                    }
+                }
+                $this->view->form = $form;
+    }
+
+    protected function _process($values)
+    {
+        // Get our authentication adapter and check credentials
+                $adapter = $this->_getAuthAdapter();
+                $adapter->setIdentity($values['username']);
+                $adapter->setCredential($values['password']);
+        
+                $auth = Zend_Auth::getInstance();
+                $result = $auth->authenticate($adapter);
+                if ($result->isValid()) {
+                    $user = $adapter->getResultRowObject();
+                    $auth->getStorage()->write($user);
+                    return true;
+                }
+                return false;
+    }
+
+    protected function _getAuthAdapter()
+    {
+        $dbAdapter = Zend_Db_Table::getDefaultAdapter();
+                $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+        
+                $authAdapter->setTableName('hna_admins')
+                    ->setIdentityColumn('login')
+                    ->setCredentialColumn('pass')
+                    ->setCredentialTreatment('MD5(?)');
+        
+                return $authAdapter;
+    }
+
+    public function logoutAction()
+    {
+        Zend_Auth::getInstance()->clearIdentity();
+        $this->_helper->redirector('index','hna');
+    }
+
+
+}
+
+
+
+
+
---
+++ application/views/helpers/LoggedAs.php
@@ -0,0 +1,23 @@
+<?php
+
+class Zend_View_Helper_LoggedAs extends Zend_View_Helper_Abstract
+{
+    public function loggedAs ()
+    {
+        $auth = Zend_Auth::getInstance();
+        if ($auth->hasIdentity()) {
+            $username = $auth->getIdentity()->login;
+            $logoutUrl = $this->view->url(array('controller'=>'admin', 'action'=>'logout'), null, true);
+            return 'Welcome ' . $username .  '. <a href="'.$logoutUrl.'">Logout</a>';
+        }
+
+        $request = Zend_Controller_Front::getInstance()->getRequest();
+        $controller = $request->getControllerName();
+        $action = $request->getActionName();
+        if($controller == 'admin' && $action == 'login') {
+            return '';
+        }
+        $loginUrl = $this->view->url(array('controller'=>'admin', 'action'=>'login'));
+        return '<a href="'.$loginUrl.'">Login</a>';
+    }
+}
