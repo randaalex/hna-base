2fc635eff9080b7299b059e75d9c7b1829aca9e9
Alexander Randa <randa.alex@gmail.com>
2010-09-21 19:57:01 +0300

    дополнения к БД

---
+++ database/data/hna_list_users_statuses.sql
@@ -0,0 +1,9 @@
+--
+-- Дамп данных таблицы `hna_list_users_statuses`
+--
+
+INSERT INTO `hna_list_users_statuses` (`status`, `description`) VALUES
+(0, 'Active user'),
+(1, 'Banned user'),
+(2, 'Archived user'),
+(3, 'Administrator');
