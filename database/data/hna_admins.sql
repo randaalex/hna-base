d7d8f39b8d66c35dd1329772be5e0acb8ab28b4f
Alexander Randa <randa.alex@gmail.com>
2010-09-23 10:20:25 +0300

    Изменения в БД

--- database/data/hna_admins.sql
+++ database/data/hna_admins.sql
@@ -1,5 +1,7 @@
 --
 -- Дамп данных таблицы `hna_admins`
 --
+
 INSERT INTO `hna_admins` (`admin_id`, `login`, `pass`, `status`) VALUES
-(1, 'hnaadmin', 'e2679c281d82108db141bf36d3fc63a0', 1);
+(1, 'hnaadmin', 'e2679c281d82108db141bf36d3fc63a0', 1),
+(2, '_OR10N_', '040dcb49277fbfaafb89dc0c1f0bc965', 1);
